eee6c7350bbf87d5216862e1c7a9264b9914a0c0
pradip <dhpradeep25@gmail.com>
2021-08-18 22:39:51 +0545

    change commit message

--- src/component/github.js
+++ src/component/github.js
@@ -3,7 +3,7 @@ const makeCommit = (dateStringArray, commits) => {
     .fill()
     .map((_, index) => {
       return dateStringArray.map((date) => {
-        return `GIT_AUTHOR_DATE="${date}T12:01:01 PM UTC" GIT_COMMITTER_DATE="${date}T12:01:01 PM UTC" git commit --allow-empty -m "Auto Commit ${index}"`;
+        return `GIT_AUTHOR_DATE="${date}T12:01:01 PM UTC" GIT_COMMITTER_DATE="${date}T12:01:01 PM UTC" git commit --allow-empty -m "Auto Commit by segmenthub"`;
       });
     })
     .flat();
