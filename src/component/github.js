660b621ce4a8044826a26613e3d8fe6330644896
pradip <dhpradeep25@gmail.com>
2021-08-19 21:15:38 +0545

    change some logic

--- src/component/github.js
+++ src/component/github.js
@@ -1,10 +1,11 @@
 const makeCommit = (dateStringArray, commits) => {
-  return Array(commits)
-    .fill()
-    .map((_, index) => {
-      return dateStringArray.map((date) => {
-        return `GIT_AUTHOR_DATE="${date}T12:01:01 PM UTC" GIT_COMMITTER_DATE="${date}T12:01:01 PM UTC" git commit --allow-empty -m "Auto Commit by segmenthub"`;
-      });
+  return dateStringArray
+    .map((date) => {
+      return Array(commits)
+        .fill()
+        .map((_, __) => {
+          return `GIT_AUTHOR_DATE="${date}T12:01:01 PM UTC" GIT_COMMITTER_DATE="${date}T12:01:01 PM UTC" git commit --allow-empty -m "Auto Commit by segmenthub" > /dev/null`;
+        });
     })
     .flat();
 };
